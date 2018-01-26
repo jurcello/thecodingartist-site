9e64c13ea67bb166c27c5b7278d38cd71aa8ca17
Jur de Vries <info@jurdevries.nl>
2018-01-26 12:43:17 +0100

    Added body field to home page.

--- codingartist/home/models.py
+++ codingartist/home/models.py
@@ -3,7 +3,18 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    description = RichTextField(blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname='full')
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(HomePage, self).get_context(request, *args, **kwargs)
+        context['page'] = self
+        return context
