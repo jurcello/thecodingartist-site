9d6a6231343254d0500b4f89a9d41f0a6031c08c
Jur de Vries <info@jurdevries.nl>
2019-02-11 17:26:27 +0100

    Added some default jquery inspired javascript.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 import init from './plain/main'
 
-new Vue({
-  el: '#app',
-  render: h => h(App),
-  components: [
-      App
-  ]
-});
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+//   components: [
+//       App
+//   ]
+// });
 
 init();
 
--- frontend/src/js/plain/main.js
+++ frontend/src/js/plain/main.js
@@ -1,7 +1,21 @@
-export default  () => {
-  function sayHello() {
-    alert('hallo');
-  }
+import $ from 'jquery';
+import 'jquery.scrollintoview';
 
-  sayHello();
+function scrollToDiv(div){
+  $('html, body').animate({
+    scrollTop: $(div).offset().top
+  }, 500);
+}
+
+
+export default  () => {
+  $("#show-more").click( event => {
+    event.preventDefault();
+    scrollToDiv($("#scroll-target"));
+  });
+
+  $("#scroll-to-top").click(event => {
+    event.preventDefault();
+    scrollToDiv($(".main").first());
+  });
 }
