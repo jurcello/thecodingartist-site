fb775f945d63706379f703a6dc434f839e24783b
Jur de Vries <info@jurdevries.nl>
2019-02-19 17:11:58 +0100

    Css tweaks and changes in media url.

--- codingartist/codingartist/settings/base.py
+++ codingartist/codingartist/settings/base.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/assets/'
 
 COMPRESS_ROOT = MEDIA_ROOT
 
